23631d41c4f8430b419b095a991256183c6b6cea
vampyrsoda <72835263+vampyrsoda@users.noreply.github.com>
2021-03-22 22:52:45 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import React from "react";
+import {ListaDeNotas} from "./components/ListaDeNotas"
 import logo from './logo.svg';
 import './App.css';
 
+///App Render Zone
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva a sua nota..."/>
+        <button>Criar nota</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
   );
 }
 
