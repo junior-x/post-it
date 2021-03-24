ca958bf52f0580d885b7a8eacf78aa3d4365fda1
vampyrsoda <72835263+vampyrsoda@users.noreply.github.com>
2021-03-23 22:55:13 -0300

    Create setup.jsx

---
+++ .vscode/setup.jsx
@@ -0,0 +1,12 @@
+On my visual studio code did not see the ".JSX" extenxions file.
+
+###To get a quickly fiz for that you can try this:
+
+###Go to ###settings.jsx
+
+###Then #copy  &   #paste  the code from here
+
+###or
+
+###If you want you can just replace the file in you .vscode/config
+###Keep in mind if you replace your original files you may ***LOSE ALL YOUR SETUP!***
