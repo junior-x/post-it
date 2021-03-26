4d028841e29b52c85877fd14a334517ec6d580ae
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-03-25 22:44:17 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import "./assets/index.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.notas = [];
+    this.state = {
+      notas: [],
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+
+    this.notas.push(novaNota);
+    this.setState(novoEstado);
+    ///console.log(this.notas.length);
+    ///console.log(`uma nova nota foi criada` + titulo + " " + texto)
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
+}
+
+export default App;
