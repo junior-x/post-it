44aa74118a62739cf73f57adf40077af71238f6c
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-03-25 22:02:25 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.notas = [];
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    this.notas.push(novaNota);
+    console.log(this.notas.length);
+    ///console.log(`uma nova nota foi criada` + titulo + " " + texto)
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas}/>
       </section>
     );
   }
