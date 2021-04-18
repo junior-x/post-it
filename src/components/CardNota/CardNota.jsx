2b644920d4ab1adf074f2924882b7abd6d6c7635
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-18 20:14:57 -0300

    Add files via upload

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -13,8 +13,8 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <DeleteSVG onClick={this.apagar.bind(this)}/>
           <h4>{this.props.categoria}</h4>
+          <DeleteSVG onClick={this.apagar.bind(this)} className="card-nota_delete"/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
 
